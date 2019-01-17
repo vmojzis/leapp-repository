6a87c44dead216f7140f99177b4820cb280d965d
Petr Stodulka <pstodulk@redhat.com>
2019-01-17 15:48:15 +0100

    add selinuxmodule model

---
+++ repos/system_upgrade/el7toel8/models/selinux.py
@@ -0,0 +1,15 @@
+from leapp.models import Model, fields
+from leapp.topics import SystemInfoTopic
+
+class SelinuxModule(Model):
+    topic = SystemInfoTopic
+    name = fields.String()
+    priority = fields.Integer()
+    content = fieds.String()
+
+
+class SelinuxModules(Model):
+    topic = SystemInfoTopic
+    modules = fields.List(fields.Model(SelinuxModule))
+
+
