9459c03725774d9206884fd6835117c9efcb9720
Petr Stodulka <pstodulk@redhat.com>
2019-01-17 16:22:48 +0100

    sfS

--- repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
+++ repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
@@ -1,13 +1,27 @@
 from leapp.actors import Actor
-
+from leapp.models import SelinuxModules, SelinuxModule, SystemFacts
+from leapp.tags import FactsCollectionPhaseTag, IPUWorkflowTag
+from leapp.libraries.stdlib import call
 
 class Selinuxcontentscanner(Actor):
     name = 'selinuxcontentscanner'
     description = 'No description has been provided for the selinuxcontentscanner actor.'
-    consumes = ()
-    produces = (SelinuxModels)
-    tags = ()
+    consumes = (SystemFacts)
+    produces = (SelinuxModules,)
+    tags = (FactsCollectionPhaseTag, IPUWorkflowTag)
 
     def process(self):
-        pass
+        for fact in self.consume(SystemFacts):
+            if fact.selinux.enabled is False:
+                return
+
+        cmd = [ 'semanage', 'export' ]
+        stdout = call(cmd)
+        semodules = SelinuxModules(
+            modules=SelinuxModule(
+                name="nn",
+                priority=1,
+                content=stdout
+        ),)
+        self.produce(semodules)
 
