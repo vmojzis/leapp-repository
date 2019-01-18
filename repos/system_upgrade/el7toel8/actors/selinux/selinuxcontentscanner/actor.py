60562fe7d10c1e06b886da10512bb8890c437d20
Vit Mojzis <vmojzis@redhat.com>
2019-01-18 15:31:55 +0100

    Add production of selinux modules

--- repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
+++ repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
@@ -1,7 +1,10 @@
 from leapp.actors import Actor
-from leapp.models import SelinuxModules, SelinuxModule, SystemFacts
+from leapp.models import SelinuxModules, SelinuxModule, SelinuxCustom, SystemFacts
 from leapp.tags import FactsPhaseTag, IPUWorkflowTag
 from leapp.libraries.stdlib import call
+import subprocess
+import re
+import os
 
 class SelinuxContentScanner(Actor):
     name = 'selinuxcontentscanner'
@@ -11,17 +14,84 @@ class SelinuxContentScanner(Actor):
     tags = (FactsPhaseTag, IPUWorkflowTag, )
 
     def process(self):
+        # exit if SELinux is disabled
         for fact in self.consume(SystemFacts):
             if fact.selinux.enabled is False:
                 return
 
-        cmd = [ 'semanage', 'export' ]
-        stdout = call(cmd, split=False)
-        semodules = SelinuxModules(
-            modules=[SelinuxModule(
-                name="nn",
-                priority=1,
-                content=stdout
-        )],)
-        self.produce(semodules)
+        semodule_list = getSelinuxModules()
 
+        self.produce(SelinuxModules(modules=semodule_list))
+
+        try:
+            semanage = call(['semanage', 'export'], split=True)
+        except subprocess.CalledProcessError:
+            return
+
+        self.produce(SelinuxCustom(commands=semanage))
+
+        #cmd = [ 'semanage', 'export' ]
+        #stdout = call(cmd, split=False)
+        #semodules = SelinuxModules(
+        #    modules=[SelinuxModule(
+        #        name="nn",
+        #        priority=1,
+        #        content=stdout
+        #)],)
+        #self.produce(semodules)
+
+
+    def parseSemodule(modules_str):
+        """Parse list of modules into list of tuples (name,priority)"""
+        modules = []
+        for module in modules_str:
+            m = re.match('([0-9]+)\s+(\w+)\s+(\w+)\s*\Z', module)
+            if not m:
+                #invalid output of "semodule -lfull"
+                break
+            modules.append((m.group(2), m.group(1)))
+
+        return modules
+
+    def getSelinuxModules():
+        try:
+            semodule = call(['semodule', '-lfull'], split=True)
+        except subprocess.CalledProcessError:
+            return
+
+        modules = parseSemodule(semodule)
+        semodule_list = []
+
+        # modules need to be extracted into cil files
+        # cd to /tmp/selinux and save working directory so that we can return there
+        wd = os.getcwd()
+        os.mkdir("/tmp/selinux")
+        os.chdir("/tmp/selinux")
+
+        for (name, priority) in modules:
+            if priority == "200":
+                #TODO - request "name-selinux" to be installed
+                continue
+            if priority == "100":
+                #module from selinux-policy-* package - skipping
+                continue
+            # extract custom module and save it to SelinuxModule object
+            try:
+                call(['semodule', '-c', '-X', priority, '-E', name])
+                module_content = call(['cat', name + ".cil"], split=True)
+                semodule_list.append(SelinuxModule(
+                    name=name,
+                    priority=int(priority),
+                    content=module_content
+                    )
+                )
+                # clean-up
+                os.remove(name + ".cil")
+
+            except subprocess.CalledProcessError:
+                continue
+        # clean-up
+        os.rmdir("/tmp/selinux")
+        os.chdir(wd)
+
+        return semodule_list
--- repos/system_upgrade/el7toel8/models/selinux.py
+++ repos/system_upgrade/el7toel8/models/selinux.py
@@ -2,14 +2,19 @@ from leapp.models import Model, fields
 from leapp.topics import SystemInfoTopic
 
 class SelinuxModule(Model):
+    """SELinux module in cil including priority"""
     topic = SystemInfoTopic
     name = fields.String()
     priority = fields.Integer()
-    content = fields.String()
-
+    content = fields.List(fields.String())
 
 class SelinuxModules(Model):
+    """List of custom selinux modules (priority != 100,200)"""
     topic = SystemInfoTopic
     modules = fields.List(fields.Model(SelinuxModule))
 
+class SelinuxCustom(Model):
+    """SELinux customizations returned by semanage export"""
+    topic = SystemInfoTopic
+    commands = fields.List(fields.String)
 
