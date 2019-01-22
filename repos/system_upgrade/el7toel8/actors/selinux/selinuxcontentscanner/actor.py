1423453867b0780261c00845ec0a8860c675f381
Vit Mojzis <vmojzis@redhat.com>
2019-01-22 17:34:13 +0100

    selinuxcontentscanner: Move helper methods to the class
    This is necessary for log reporting.

--- repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
+++ repos/system_upgrade/el7toel8/actors/selinux/selinuxcontentscanner/actor.py
@@ -20,103 +20,6 @@ SED_COMMAND='/' + '\|'.join(REMOVED_TYPES_) + '/s/^/;/g'
 CONTAINER_TYPES="|".join(["container_connect_any","container_runtime_t","container_runtime_exec_t","spc_t","container_auth_t","container_auth_exec_t","spc_var_run_t","container_var_lib_t","container_home_t","container_config_t","container_lock_t","container_log_t","container_runtime_tmp_t","container_runtime_tmpfs_t","container_var_run_t","container_plugin_var_run_t","container_unit_file_t","container_devpts_t","container_share_t","container_port_t","container_build_t","container_logreader_t","docker_log_t","docker_tmpfs_t","docker_share_t","docker_t","docker_lock_t","docker_home_t","docker_exec_t","docker_unit_file_t","docker_devpts_t","docker_config_t","docker_tmp_t","docker_auth_exec_t","docker_plugin_var_run_t","docker_port_t","docker_auth_t","docker_var_run_t","docker_var_lib_t","container_domain","container_net_domain"])
 
 
-def checkModule(name):
-    """ Check if module contains one of removed types.
-        If so, comment out corresponding lines and return them.
-    """
-    try:
-        removed = call(['grep', '-w', '-E', REMOVED_TYPES, name], split=True)
-        call(['sed', '-i', SED_COMMAND, name])
-        return removed
-    except subprocess.CalledProcessError:
-        return []
-
-def parseSemodule(modules_str):
-    """Parse list of modules into list of tuples (name,priority)"""
-    modules = []
-    for module in modules_str:
-        # Matching line such as "100 zebra             pp "
-        # "<priority> <module name>    <module type - pp/cil> "
-        m = re.match('([0-9]+)\s+(\w+)\s+(\w+)\s*\Z', module)
-        if not m:
-            #invalid output of "semodule -lfull"
-            break
-        modules.append((m.group(2), m.group(1)))
-
-    return modules
-
-def getSELinuxModules():
-    try:
-        semodule = call(['semodule', '-lfull'], split=True)
-    except subprocess.CalledProcessError:
-        return
-
-    modules = parseSemodule(semodule)
-    semodule_list = []
-
-    # modules need to be extracted into cil files
-    # cd to /tmp/selinux and save working directory so that we can return there
-    try:
-        wd = os.getcwd()
-        rmtree(WORKING_DIRECTORY)
-        os.mkdir(WORKING_DIRECTORY)
-        os.chdir(WORKING_DIRECTORY)
-    except OSError:
-        self.log.info("Failed to access working directory! Aborting.")
-        #return
-
-    for (name, priority) in modules:
-        if priority == "200":
-            #TODO - request "name-selinux" to be installed
-            continue
-        if priority == "100":
-            #module from selinux-policy-* package - skipping
-            continue
-        # extract custom module and save it to SELinuxModule object
-        try:
-            call(['semodule', '-c', '-X', priority, '-E', name])
-            # check if the module contains invalid types and remove them if so
-            removed = checkModule(name + ".cil")
-            # get content of the module
-            module_content = call(['cat', name + ".cil"], split=False)
-
-            semodule_list.append(SELinuxModule(
-                name=name,
-                priority=int(priority),
-                content=module_content,
-                removed=removed
-                )
-            )
-        except subprocess.CalledProcessError:
-            continue
-        # rename the cil module file so that it does not clash
-        # with the same module on different priority
-        try:
-            os.rename(name + ".cil", name + "_" + str(priority))
-        except OSError:
-            self.log.info("Failed to rename module file.")
-    # this is necessary for check if container-selinux needs to be installed
-    try:
-        call(['semanage', 'export', '-f', 'semanage'])
-    except subprocess.CalledProcessError:
-        pass
-    # Check if modules contain any type, attribute, or boolean contained in container-selinux and install it if so
-    # This is necessary since container policy module is part of selinux-policy-targeted in RHEL 7 (but not in RHEL 8) 
-    try:
-        semodule = call(['grep', '-w', '-r', '-E', CONTAINER_TYPES], split=False)
-        #TODO - request "container-selinux" to be installed
-    except subprocess.CalledProcessError:
-        # expected, ignore exception
-        pass
-        
-    try:
-        rmtree(WORKING_DIRECTORY)
-        os.chdir(wd)
-    except OSError:
-        pass
-
-    return semodule_list
-
 class SELinuxContentScanner(Actor):
     name = 'selinuxcontentscanner'
     description = 'No description has been provided for the selinuxcontentscanner actor.'
@@ -169,3 +72,104 @@ class SELinuxContentScanner(Actor):
         #self.produce(semodules)
 
 
+
+    def checkModule(self, name):
+        """ Check if module contains one of removed types.
+            If so, comment out corresponding lines and return them.
+        """
+        try:
+            removed = call(['grep', '-w', '-E', REMOVED_TYPES, name], split=True)
+            call(['sed', '-i', SED_COMMAND, name])
+            return removed
+        except subprocess.CalledProcessError:
+            return []
+
+    def parseSemodule(self, modules_str):
+        """Parse list of modules into list of tuples (name,priority)"""
+        modules = []
+        for module in modules_str:
+            # Matching line such as "100 zebra             pp "
+            # "<priority> <module name>    <module type - pp/cil> "
+            m = re.match('([0-9]+)\s+(\w+)\s+(\w+)\s*\Z', module)
+            if not m:
+                #invalid output of "semodule -lfull"
+                break
+            modules.append((m.group(2), m.group(1)))
+
+        return modules
+
+    def getSELinuxModules(self):
+        try:
+            semodule = call(['semodule', '-lfull'], split=True)
+        except subprocess.CalledProcessError:
+            return
+
+        modules = parseSemodule(semodule)
+        semodule_list = []
+
+        # modules need to be extracted into cil files
+        # cd to /tmp/selinux and save working directory so that we can return there
+        try:
+            rmtree(WORKING_DIRECTORY)
+        except OSError:
+            #expected
+            pass
+        try:
+            wd = os.getcwd()
+            os.mkdir(WORKING_DIRECTORY)
+            os.chdir(WORKING_DIRECTORY)
+        except OSError:
+            self.log.info("Failed to access working directory! Aborting.")
+            return []
+
+        for (name, priority) in modules:
+            if priority == "200":
+                #TODO - request "name-selinux" to be installed
+                continue
+            if priority == "100":
+                #module from selinux-policy-* package - skipping
+                continue
+            # extract custom module and save it to SELinuxModule object
+            try:
+                call(['semodule', '-c', '-X', priority, '-E', name])
+                # check if the module contains invalid types and remove them if so
+                removed = self.checkModule(name + ".cil")
+                # get content of the module
+                module_content = call(['cat', name + ".cil"], split=False)
+
+                semodule_list.append(SELinuxModule(
+                    name=name,
+                    priority=int(priority),
+                    content=module_content,
+                    removed=removed
+                    )
+                )
+            except subprocess.CalledProcessError:
+                continue
+            # rename the cil module file so that it does not clash
+            # with the same module on different priority
+            try:
+                os.rename(name + ".cil", name + "_" + str(priority))
+            except OSError:
+                self.log.info("Failed to rename module file.")
+        # this is necessary for check if container-selinux needs to be installed
+        try:
+            call(['semanage', 'export', '-f', 'semanage'])
+        except subprocess.CalledProcessError:
+            pass
+        # Check if modules contain any type, attribute, or boolean contained in container-selinux and install it if so
+        # This is necessary since container policy module is part of selinux-policy-targeted in RHEL 7 (but not in RHEL 8)
+        try:
+            semodule = call(['grep', '-w', '-r', '-E', CONTAINER_TYPES], split=False)
+            #TODO - request "container-selinux" to be installed
+        except subprocess.CalledProcessError:
+            # expected, ignore exception
+            pass
+
+        try:
+            rmtree(WORKING_DIRECTORY)
+            os.chdir(wd)
+        except OSError:
+            pass
+
+        return semodule_list
