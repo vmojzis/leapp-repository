86f0e1c89c438f84ddf6f4c3884d58d92526e312
Vit Mojzis <vmojzis@redhat.com>
2019-02-13 13:55:35 +0100

    Fix minor issues
    - Add doc strings
    - Fix order of includes
    - Rewrite log messages to be more readable
    - Change string formatting
    - Stop importing subprocess
    - Replace "call" with "run"
    - Add SELinuxRequestRPMs model to track requested RPMs
    
    Signed-off-by: Vit Mojzis <vmojzis@redhat.com>

--- repos/system_upgrade/el7toel8/actors/selinux/selinuxapplycustom/actor.py
+++ repos/system_upgrade/el7toel8/actors/selinux/selinuxapplycustom/actor.py
@@ -1,16 +1,22 @@
-from leapp.actors import Actor
-from leapp.models import SELinuxModules, SELinuxCustom, CheckResult
-from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
-from leapp.libraries.stdlib import call
-import subprocess
 import os
 from shutil import rmtree
+
+from leapp.actors import Actor
+from leapp.models import SELinuxModules, SELinuxCustom, SELinuxRequestRPMs, CheckResult
+from leapp.tags import ApplicationsPhaseTag, IPUWorkflowTag
+from leapp.libraries.stdlib import run, CalledProcessError
 
 WORKING_DIRECTORY = '/tmp/selinux/'
 
 class SELinuxApplyCustom(Actor):
+    '''
+    Re-apply SELinux customizations from RHEL-7 installation
+
+    Re-apply SELinux policy customizations (custom policy modules and changes
+    introduced by semanage). Any changes (due to incompatiblity with RHEL-8
+    SELinux policy) are reported to user.
+    '''
     name = 'selinuxapplycustom'
-    description = 'No description has been provided for the selinuxapplycustom actor.'
     consumes = (SELinuxCustom, SELinuxModules, )
     produces = (CheckResult, )
     tags = (ApplicationsPhaseTag, IPUWorkflowTag, )
@@ -21,7 +27,7 @@ class SELinuxApplyCustom(Actor):
             # clear working directory
             rmtree(WORKING_DIRECTORY)
         except OSError:
-            #expected
+            # expected
             pass
         try:
             os.mkdir(WORKING_DIRECTORY)
@@ -31,13 +37,10 @@ class SELinuxApplyCustom(Actor):
 
         # import custom SElinux modules
         for semodules in self.consume(SELinuxModules):
-            self.log.info("Processing custom SELinux policy modules. Count: " +
-                str(len(semodules.modules))
-            )
+            self.log.info("Processing custom SELinux policy modules. Count: %d." % len(semodules.modules))
             for module in semodules.modules:
-                cil_filename = WORKING_DIRECTORY + module.name + ".cil"
-                self.log.info("Installing " + module.name
-                 + " on priority " + str(module.priority) + ".")
+                cil_filename = "%s%s.cil" % (WORKING_DIRECTORY, module.name)
+                self.log.info("Installing module %s on priority %d." % (module.name, module.priority))
                 if module.removed:
                     self.log.info("The following lines where removed because of incompatibility: ")
                     self.log.info('\n'.join(module.removed))
@@ -46,24 +49,26 @@ class SELinuxApplyCustom(Actor):
                     with open(cil_filename, 'w') as file:
                         file.write(module.content)
                 except OSError as e:
-                    self.log.info("Error writing " + cil_filename + " :" + e.strerror)
+                    self.log.info("Error writing %s : %s" % (cil_filename, e.strerror))
                     continue
 
                 try:
-                    semanage = call([
+                    semanage = run([
                         'semodule',
                         '-X',
                         str(module.priority),
                         '-i',
                         cil_filename]
                     )
-                except subprocess.CalledProcessError as e:
-                    self.log.info("Error installing module: " + e.strerror)
+                except CalledProcessError as e:
+                    self.log.info("Error installing module: %s" % e.strerror)
+                    # TODO - save the failed module to /etc/selinux ?
+                    # currently it is still left in the old policy store
                     pass
                 try:
                     os.remove(cil_filename)
                 except OSError:
-                    self.log.info("Error removing module file")
+                    self.log.info("Error removing module file.")
         # import SELinux customizations collected by "semanage export"
         for custom in self.consume(SELinuxCustom):
             self.log.info('Importing SELinux customizations collected by "semanage export".')
@@ -73,11 +78,11 @@ class SELinuxApplyCustom(Actor):
                 with open(semanage_filename, 'w') as file:
                     file.write('\n'.join(custom.commands))
             except OSError as e:
-                self.log.info("Error writing SELinux customizations:" + e.strerror)
+                self.log.info("Error writing SELinux customizations: %s" % e.strerror)
             # import customizations
             try:
-                call(['semanage', 'import', '-f', semanage_filename])
-            except subprocess.CalledProcessError:
+                run(['semanage', 'import', '-f', semanage_filename])
+            except CalledProcessError:
                 continue
             # clean-up
             try:
@@ -91,6 +96,12 @@ class SELinuxApplyCustom(Actor):
         except OSError:
             pass
 
+        # TODO - Verify that all RPM packages reqested by selinux actors are installed
+        for rpms in self.consume(SELinuxRequestRPMs):
+            self.log.info("Verifying selinux-related RPMs requested before upgrade.")
+            self.log.info("To keep: %s \n To install: %s" % (", ".join(rpms.to_keep), ", ".join(rpms.to_install)))
+
+        # TODO - will be rewritten after LEAPP team rewrites reporting
         self.log.info("SElinux customizations reapplied successfully.")
         self.produce(
            CheckResult(
